f98ab7f4896676cd8f6f28f57ae9c18f51a45821
root <root@file.europe>
2025-08-28 17:44:39 +0000

    updating

--- excalidraw-app/App.tsx
+++ excalidraw-app/App.tsx
@@ -335,7 +335,7 @@ const initializeScene = async (opts: {
 
 const ExcalidrawWrapper = () => {
   const [errorMessage, setErrorMessage] = useState("");
-  const isCollabDisabled = isRunningInIframe();
+  const isCollabDisabled = isRunningInIframe() && !isSelfEmbedding;
 
   const { editorTheme, appTheme, setAppTheme } = useHandleAppTheme();
 
@@ -735,21 +735,21 @@ const ExcalidrawWrapper = () => {
   // browsers generally prevent infinite self-embedding, there are
   // cases where it still happens, and while we disallow self-embedding
   // by not whitelisting our own origin, this serves as an additional guard
-  if (isSelfEmbedding) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          textAlign: "center",
-          height: "100%",
-        }}
-      >
-        <h1>I'm not a pretzel!</h1>
-      </div>
-    );
-  }
+//  if (isSelfEmbedding) {
+//    return (
+//      <div
+ //       style={{
+ //         display: "flex",
+ //         alignItems: "center",
+ //         justifyContent: "center",
+ //         textAlign: "center",
+ //         height: "100%",
+ //       }}
+ //     >
+ //       <h1>I'm not a pretzel!</h1>
+ //     </div>
+ //   );
+ // }
 
   const ExcalidrawPlusCommand = {
     label: "Excalidraw+",
